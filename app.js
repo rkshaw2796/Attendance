7ef4a8c13a6a54f273d6e6a9f4b1eb0cec8ed734
Rahul Shaw <rkshaw2796@gmail.com>
2021-09-07 19:14:51 +0530

    Second commit: Login working with stored JSON data and send success and failed message

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+const JSONBuffer = fs.readFileSync("./public/data/logindata.json");
+// console.log(JSON.parse(JSONBuffer.toString()).user[1].name);
+var loginlist = JSON.parse(JSONBuffer.toString()).user;
+// console.log(loginlist.length);
+
+
+
+
 const port = 5000;
 let a;
 
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/signin",(req,res) =>{
-    res.redirect("/");
+    // res.redirect("/");
     a = req.query;
     console.log(a);
+    let check = false;
+
+    for(i =0; i < loginlist.length; i++){
+        if(a.username == loginlist[i].name && a.password == loginlist[i].password)
+        check = true;
+    }
+    if(check){
+        res.send("<h1>Success!</h1>");
+    }
+    else{
+        res.send("<h1>Failed</h1>");
+    }
+    // let b = JSON.stringify(a);
+    // console.log(b);
+    // console.log(JSON.parse(b));
+    
 });
 
 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,13 @@
-Initialised the empty git respository in folder Login_Page --command git init
+Initialised the empty local git respository in folder Login_Page --command git init
+
+Configure the global variable:
+git config --global user.name "Rahul Shaw"
+git config --global user.email "rkshaw2796@gmail.com"
+
+Run the below command to connect GitHub:
+echo "# Attendance" >> README.md
+git add .                               --reason: Moving all files and folders from working directory to staging stage
+git commit -m "first commit"            --reason: commit all the staging files to commit stage.
+git branch -M main                      --reason: cretae main branch
+git remote add origin https://github.com/rkshaw2796/Attendance.git     --reason: created remote origin and added the remote github url
+git push -u origin main                 --reason: pushing the local repository to remote repository
